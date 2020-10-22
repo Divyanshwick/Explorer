c3e12a4ac1886ea89f14a95b447929bab45725e6
Divyanshwick <ddivyansh9999@gmail.com>
2020-10-22 13:46:46 +0530

    Updated

--- app.js
+++ app.js
@@ -84,18 +84,18 @@ passport.use(new GitHubStrategy({
   }
 ));
 
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/home/ideas"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-      console.log(profile);
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+// passport.use(new FacebookStrategy({
+//     clientID: process.env.FACEBOOK_APP_ID,
+//     clientSecret: process.env.FACEBOOK_APP_SECRET,
+//     callbackURL: "http://localhost:3000/auth/facebook/home/ideas"
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//       console.log(profile);
+//     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
 
 
 app.use(function(req,res,next){
